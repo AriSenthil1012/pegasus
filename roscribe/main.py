2f1ffcd8fe28537c69c065797919c81621ecde5a
AriSenthil1012 <arisenthil@diractechs.onmicrosoft.com>
2024-12-14 16:20:40 -0600

    second

---
+++ roscribe/main.py
@@ -0,0 +1,37 @@
+import os 
+import sys 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from roscribe.spec_agent import SpecAgent, load_spec_agent
+from roscribe.gen_agent import GenAgent, load_gen_agent
+from roscribe.pack_agent import PackAgent, load_pack_agent
+from roscribe.support_agent import SupportAgent
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def main(verbose=False):
+    verbose = False
+
+    spec_agent = SpecAgent(ros_distro='noetic', verbose=verbose)
+    spec_agent.spin()
+    loaded_spec_agent = load_spec_agent('spec_agent.pkl')
+    gen_agent = GenAgent(ros_distro='noetic', ros_graph_dict=loaded_spec_agent.get_ros_graph_dict(),
+                         ros_node_desc=loaded_spec_agent.get_ros_node_desc(), verbose=verbose)
+    gen_agent.spin()
+    loaded_gen_agent = load_gen_agent('gen_agent.pkl', 'spec_agent.pkl')
+    pack_agent = PackAgent(ros_distro='noetic', ros_graph_dict=loaded_spec_agent.get_ros_graph_dict(),
+                           ros_node_desc=loaded_spec_agent.get_ros_node_desc(), ros_nodes=loaded_gen_agent.nodes,
+                           project_name=loaded_gen_agent.project_name, ws_name=loaded_gen_agent.ws_name,
+                           verbose=verbose)
+    pack_agent.spin()
+    loaded_pack_agent = load_pack_agent('pack_agent.pkl', 'gen_agent.pkl', 'spec_agent.pkl')
+    support_agent = SupportAgent(ros_distro='noetic', ros_graph_dict=loaded_spec_agent.get_ros_graph_dict(),
+                                 ros_nodes=loaded_gen_agent.nodes, project_name=loaded_gen_agent.project_name,
+                                 ws_name=loaded_gen_agent.ws_name, dependencies=loaded_pack_agent.dependencies,
+                                 package=loaded_pack_agent.package, verbose=verbose)
+    support_agent.spin()
+
+
+if __name__ == '__main__':
+    main()
--- roscribe/support_agent.py
+++ roscribe/support_agent.py
@@ -3,7 +3,7 @@ import os
 
 from langchain_openai import ChatOpenAI
 from langchain.agents import AgentExecutor, create_openai_tools_agent
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 
 # Add the parent directory (pegasus) to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -11,4 +11,46 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from roscribe.tools import get_rag_tools, get_file_tool
 from roscribe.prompts import get_support_agent_prompt
 
-print("hello")
+import roscribe.ui as ui
+
+
+class SupportAgent:
+    def __init__(self, ros_distro, ros_graph_dict, ros_nodes, project_name, ws_name, package, dependencies,
+                 model_name="gpt-4o-mini", max_interaction_limit=6, verbose=False):
+        self.ros_distro = ros_distro
+        self.model_name = model_name
+        self.max_interaction_limit = max_interaction_limit
+        llm = ChatOpenAI(model_name=model_name, temperature=0.1)
+
+        self.ros_graph_dict = ros_graph_dict
+        self.nodes = ros_nodes
+
+        self.project_name = project_name
+        self.ws_name = ws_name
+
+        self.package = package
+        self.dependencies = dependencies
+
+        self.verbose = verbose
+
+        rag_tool = get_rag_tool(ros_distro)
+        load_file_tool = get_file_tool(agent=self)
+
+        tools = [rag_tool, load_file_tool]
+
+        agent_prompt = get_support_agent_prompt(agent=self)
+        agent_memory = ConversationBufferWindowMemory(memory_key="chat_history", k=max_interaction_limit,
+                                                      return_messages=True)
+        agent = create_openai_tools_agent(llm, tools, agent_prompt)
+        self.agent_executor = AgentExecutor(agent=agent, tools=tools, memory=agent_memory, verbose=verbose)
+
+    def __call__(self, human_input):
+        agent_response = self.agent_executor.invoke({"input": human_input})["output"]
+        return agent_response
+
+    def spin(self):
+        print(ui.SUPPORT_AGENT_LAUNCH_MSG)
+        while True:
+            human_input = input()
+            agent_response = self(human_input)
+            print(agent_response + "\n")
